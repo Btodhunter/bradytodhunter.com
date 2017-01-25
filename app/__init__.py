ebc5a1e45d6b315682075802fe5aba560cdc86c8
Brady Todhunter <Brady.Todhunter@wildtangent.com>
2017-01-25 12:27:34 -0800

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask_login import LoginManager
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+lm = LoginManager(app)
+
+from app import views, models
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/website.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('website startup')
